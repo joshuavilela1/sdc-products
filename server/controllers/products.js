0789642146b97d8a35dae8f8141c6ffe023a5b39
joshuavilela <joshuavilela1@gmail.com>
2022-12-31 03:23:01 -0800

    DB queries done weird layout, controllers bug

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -5,7 +5,16 @@ const {
 module.exports = {
 
   getProducts: (req, res) => {
-
+    //need page and count and default amounts
+    const page = req.query.page || 1;
+    const count = req.query.count || 5;
+    getProductsDB(page, count, (err, results) => {
+      if (!err) {
+        res.send(results).status(200);
+      } else {
+        res.send(err).status(500);
+      }
+    });
   },
 
   getProductInfo: (req, res) => {
@@ -15,7 +24,7 @@ module.exports = {
       } else {
         res.send(err).status(500);
       }
-    })
+    });
   },
 
   getStyles: (req, res) => {
@@ -25,7 +34,7 @@ module.exports = {
       } else {
         res.send(err).status(500);
       }
-    })
+    });
   },
 
   getRelated: (req, res) => {
--- server/routes.js
+++ server/routes.js
@@ -3,9 +3,11 @@ const {
   products: {getProducts, getProductInfo, getStyles, getRelated}
 } = require('./controllers');
 
-productRouter.get('/products:page?:count?', getProducts);
+// {mergeParams: true}
+
 productRouter.get('/products/:product_id', getProductInfo);
-productRouter.get('/products/:product_id/styles', getStyles);
 productRouter.get('/products/:product_id/related', getRelated);
+productRouter.get('/products/:product_id/styles', getStyles);
+productRouter.get('/products', getProducts);
 
 module.exports = productRouter;
